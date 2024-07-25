75aa52a2597b9c5dfb88becb6747ff7ab99add9f
PhuongBui712 <Buitanphuong712@gmail.com>
2024-07-25 21:31:36 +0700

    feat: add global variable

--- src/global_var.py
+++ src/global_var.py
@@ -1,5 +1,6 @@
 CAMBRIDGE_URL = 'https://dictionary.cambridge.org'
 OXFORD_URL = 'https://www.oxfordlearnersdictionaries.com'
-CSV_HEADER = ['Vocabulary', 'Type', 'Cloze', 'Phonetic', 'Audio', 'English meaning', 'Vietnamese meaning', 'Example']
+CSV_HEADER = ['vocabulary', 'type', 'vietnamese meaning', 'cloze', 'phonetic', 'pronounce', 'image', 'english meaning', 'example']
+REORDERED_HEADER = ['vocabulary', 'type', 'cloze', 'phonetic', 'pronounce', 'image', 'vietnamese meaning', 'english meaning', 'example']
 US_PRON_PATTERN = "us pron"
 UK_PRON_PATTERN = "uk pron"
