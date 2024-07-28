11fb9b331658b9032a4f0cd63b6b16980c00b426
PhuongBui712 <Buitanphuong712@gmail.com>
2024-07-28 11:35:41 +0700

    refactor: upadte global variable

--- src/global_var.py
+++ src/global_var.py
@@ -2,6 +2,6 @@ CAMBRIDGE_URL = 'https://dictionary.cambridge.org'
 OXFORD_URL = 'https://www.oxfordlearnersdictionaries.com'
 INPUT_HEADER = ['vocabulary', 'type', 'vietnamese meaning']
 PROCESS_HEADER = ['vocabulary', 'type', 'vietnamese meaning', 'cloze', 'phonetic', 'pronounce', 'image', 'english meaning', 'example']
-REORDERED_HEADER = ['vocabulary', 'type', 'cloze', 'phonetic', 'pronounce', 'image', 'vietnamese meaning', 'english meaning', 'example']
+REORDERED_HEADER = ['vocabulary', 'type', 'cloze', 'phonetic', 'pronounce', 'image', 'english meaning', 'vietnamese meaning', 'example']
 US_PRON_PATTERN = "us pron"
 UK_PRON_PATTERN = "uk pron"
